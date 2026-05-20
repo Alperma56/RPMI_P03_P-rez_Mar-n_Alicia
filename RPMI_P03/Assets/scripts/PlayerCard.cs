c991edab337100cf1b1bd4ded7db7fe2d2fcfd28
casa <>
2026-05-20 11:52:31 +0200

    revisión

--- RPMI_P03/Assets/scripts/PlayerCard.cs
+++ RPMI_P03/Assets/scripts/PlayerCard.cs
@@ -6,23 +6,12 @@ public class PlayerCard : MonoBehaviour
     public AudioSource dropCardAS;
     public GameObject combatSystem;
 
-    void Start()
-    {
-        
-    }
-
-    
-    void Update()
-    {
-        
-    }
     private void OnMouseDrag()
     {
         //No podemos hacer que vaya a la posición de la cámara porque lee también el eje z, por lo que hay que crear una variable Vector 3 y hacer lo siguiente
         Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         newPosition.z = 0;
         transform.position = newPosition;
-
     }
 
     private void OnMouseDown()
